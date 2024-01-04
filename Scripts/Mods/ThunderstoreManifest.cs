f01d20115a0df5e44e76102597046804e1e20bff
Rune580 <runemaster580@gmail.com>
2024-01-04 15:16:39 -0500

    add ThunderstoreManifest json class

---
+++ Scripts/Mods/ThunderstoreManifest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace LethalConfig.Mods
+{
+    internal class ThunderstoreManifest
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("version_number")]
+        public string VersionNumber { get; set; }
+        
+        [JsonProperty("website_url")]
+        public string WebsiteURL { get; set; }
+        
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        
+        [JsonProperty("dependencies")]
+        public IList<string> Dependencies { get; set; }
+    }
+}

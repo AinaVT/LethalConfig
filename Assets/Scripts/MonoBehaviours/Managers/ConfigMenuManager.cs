492e5f2bd44f5ef418ac92bf556dbd6a7fbc92ca
Aina <146054046+AinaVT@users.noreply.github.com>
2024-01-06 01:18:50 -0300

    Created foundation of basic menu manager

---
+++ Assets/Scripts/MonoBehaviours/Managers/ConfigMenuManager.cs
@@ -0,0 +1,43 @@
+using LethalConfig.MonoBehaviours;
+using LethalConfig.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LethalConfig
+{
+    internal class ConfigMenuManager : MonoBehaviour
+    {
+        public ConfigMenuAudioManager menuAudio;
+
+        public ConfigMenuManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            LogUtils.LogInfo("Create Config Menu Manager!");
+            Instance = this;
+        }
+
+        private void OnDestroy()
+        {
+            LogUtils.LogInfo("Destroy Config Menu Manager!");
+            Instance = null;
+        }
+
+        public void ShowConfigMenu()
+        {
+            FindObjectOfType<ConfigMenu>().gameObject.SetActive(true);
+        }
+
+        public void HideConfigMenu() 
+        {
+            FindObjectOfType<ConfigMenu>().gameObject.SetActive(false);
+        }
+
+        public void DisplayNotification(string message)
+        {
+            // Temporary
+            LogUtils.LogInfo(message);
+        }
+    }
+}

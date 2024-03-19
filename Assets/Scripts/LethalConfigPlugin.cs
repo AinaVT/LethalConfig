0855fc18da157f14c271b928477313b7bbe3c07f
Aina <146054046+AinaVT@users.noreply.github.com>
2024-03-19 02:26:58 -0300

    Added setting to hide lethalconfig

--- Assets/Scripts/LethalConfigPlugin.cs
+++ Assets/Scripts/LethalConfigPlugin.cs
@@ -24,65 +24,25 @@ namespace LethalConfig
         private static LethalConfigPlugin instance;
         private static Harmony harmony;
 
-        enum TestEnum
-        {
-            None,
-            First,
-            Second
-        }
-
         private void Awake()
         {
             if (instance == null) instance = this;
 
             LogUtils.Init(PluginInfo.Guid);
+            Configs.Initialize(Config);
+
+            if (Configs.IsLethalConfigHidden.Value) 
+            {
+                LogUtils.LogInfo("LethalConfig is hidden and will not load.");
+                return;
+            }
+
             Assets.Init();
 
             harmony = new Harmony(PluginInfo.Guid);
             harmony.PatchAll();
 
-            CreateExampleConfigs();
-
             LogUtils.LogInfo("LethalConfig loaded!");
-        }
-
-        private void CreateExampleConfigs()
-        {
-            var intSlider = Config.Bind<int>("Example", "Int Slider", 30, new ConfigDescription("This is an integer slider. You can also type a value in the input field to the right of the slider.", new AcceptableValueRange<int>(0, 100)));
-            var floatSlider = Config.Bind<float>("Example", "Float Slider", 0.0f, new ConfigDescription("This is a float slider. You can also type a value in the input field to the right of the slider.", new AcceptableValueRange<float>(-1.0f, 1.0f)));
-            var floatStepSlider = Config.Bind<float>("Example", "Float Step Slider", 0.0f, new ConfigDescription("This is a float step slider. It set values in increments. You can also type a value in the input field to the right of the slider.", new AcceptableValueRange<float>(-1.0f, 1.0f)));
-            var boolCheckBox = Config.Bind<bool>("Example", "Bool Checkbox", false, new ConfigDescription("This is a bool checkbox."));
-            var enumDropDown = Config.Bind<TestEnum>("Example", "Enum Dropdown", TestEnum.None, new ConfigDescription("This is a enum dropdown."));
-            var textInput = Config.Bind<string>("Example", "Text Input", "Example", "This is a text input field. It can have a limit of characters too.");
-            var multiLineInput = Config.Bind<string>("Example", "Multiline Text Input", "Example", "This is a text input field. It can have a limit of characters too.");
-            var textInputDropdown = Config.Bind<string>("Example", "Text Input Dropdown", "Two", new ConfigDescription("This is a text input with an acceptable value list.", new AcceptableValueList<string>("One", "Two", "HL:Alyx")));
-            var intInput = Config.Bind<int>("Example", "Int Input", 50, "This is an integer input field.");
-            var floatInput = Config.Bind<float>("Example", "Float Input", 0.5f, "This is a float input field.");
-
-            LethalConfigManager.AddConfigItem(new IntSliderConfigItem(intSlider, requiresRestart: false));
-            LethalConfigManager.AddConfigItem(new FloatSliderConfigItem(floatSlider, requiresRestart: false));
-            LethalConfigManager.AddConfigItem(new FloatStepSliderConfigItem(floatStepSlider, new FloatStepSliderOptions() { Step = 0.1f, RequiresRestart = false, Min = -1.0f, Max = 1.0f }));
-            LethalConfigManager.AddConfigItem(new BoolCheckBoxConfigItem(boolCheckBox, requiresRestart: false));
-            LethalConfigManager.AddConfigItem(new EnumDropDownConfigItem<TestEnum>(enumDropDown, false));
-            LethalConfigManager.AddConfigItem(new TextInputFieldConfigItem(textInput, false));
-            LethalConfigManager.AddConfigItem(new TextInputFieldConfigItem(multiLineInput, new TextInputFieldOptions
-            {
-                NumberOfLines = 0,
-                TrimText = true
-            }));
-            LethalConfigManager.AddConfigItem(new GenericButtonConfigItem("Example", "Button", "This is a test button with a callback", "Open", () =>
-            {
-                ConfigMenuManager.Instance?.DisplayNotification("Buttons can be used to open custom menus or other things.", "OK");
-            }));
-            LethalConfigManager.AddConfigItem(new IntInputFieldConfigItem(intInput, new IntInputFieldOptions()
-            {
-                Max = 150
-            }));
-            LethalConfigManager.AddConfigItem(new FloatInputFieldConfigItem(floatInput, new FloatInputFieldOptions()
-            {
-                Max = 2.5f
-            }));
-            LethalConfigManager.AddConfigItem(new TextDropDownConfigItem(textInputDropdown, new TextDropDownOptions() { RequiresRestart = false }));
         }
     }
 

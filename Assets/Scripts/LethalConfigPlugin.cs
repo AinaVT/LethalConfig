1f72c23381291bc13742857776f6c0236f1aed2b
Aina <146054046+AinaVT@users.noreply.github.com>
2025-02-03 19:42:10 -0300

    Updating version to 1.4.5

--- Assets/Scripts/LethalConfigPlugin.cs
+++ Assets/Scripts/LethalConfigPlugin.cs
@@ -8,7 +8,7 @@ namespace LethalConfig
     {
         public const string Guid = "ainavt.lc.lethalconfig";
         public const string Name = "LethalConfig";
-        public const string Version = "1.4.4";
+        public const string Version = "1.4.5";
     }
 
     [BepInPlugin(PluginInfo.Guid, PluginInfo.Name, PluginInfo.Version)]
